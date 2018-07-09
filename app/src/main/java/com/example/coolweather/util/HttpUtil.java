8399124a5e3fc400e6367e460b8b62a545612594
zxgzlx <zxgzlx@163.com>
2018-07-09 23:05:40 +0800

    添加工具类

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.util;
+
+import javax.security.auth.callback.Callback;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        okHttpClient.newCall(request).enqueue(callback);
+    }
+}
